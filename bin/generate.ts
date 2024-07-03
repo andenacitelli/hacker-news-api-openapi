79e739951af5dfba248bb14f9893ef758ed27566
Anden Acitelli <a.acitelli@gmail.com>
2024-07-03 18:04:37 -0500

    support remaining fields

--- bin/generate.ts
+++ bin/generate.ts
@@ -56,6 +56,94 @@ registry.registerPath({
   },
 });
 
+registry.registerPath({
+  method: "get",
+  operationId: "getMaxItem",
+  path: "/maxitem.json",
+  summary: "Retrieve the current largest item id.",
+  responses: {
+    200: {
+      description: "Success",
+      content: {
+        "application/json": {
+          schema: z.number().int(),
+        },
+      },
+    },
+  },
+});
+
+for (const { operationId, path, summary } of [
+  {
+    operationId: "topstories",
+    path: "/topstories.json",
+    summary: "Retrieve a list of the 500 top stories",
+  },
+  {
+    operationId: "newstories",
+    path: "/newstories.json",
+    summary: "Retrieve a list of the 500 newest stories",
+  },
+  {
+    operationId: "beststories",
+    path: "/beststories.json",
+    summary: "Retrieve a list of the 500 best stories",
+  },
+  {
+    operationId: "askstories",
+    path: "/askstories.json",
+    summary: "Retrieve a list of 'Ask HN' stories",
+  },
+  {
+    operationId: "showstories",
+    path: "/showstories.json",
+    summary: "Retrieves list of 'Show HN' stories",
+  },
+  {
+    operationId: "jobstories",
+    path: "/jobstories.json",
+    summary:
+      "Retrieve list of 'Who's hiring' and 'Who wants to be hired' stories",
+  },
+]) {
+  registry.registerPath({
+    method: "get",
+    operationId,
+    path,
+    summary,
+    responses: {
+      200: {
+        description: "Success",
+        content: {
+          "application/json": {
+            schema: z.array(z.number().int()),
+          },
+        },
+      },
+    },
+  });
+}
+
+registry.registerPath({
+  method: "get",
+  operationId: "updates",
+  path: "/updates.json",
+  summary: "Get a list of updates to items and profiles.",
+  responses: {
+    200: {
+      description: "Success",
+      content: {
+        "application/json": {
+          schema: z.object({
+            items: z.array(z.number().int()),
+            profiles: z.array(z.number().int()),
+          }),
+        },
+      },
+    },
+  },
+});
+
 const generator = new OpenApiGeneratorV31(registry.definitions);
 
 fs.writeFileSync(
